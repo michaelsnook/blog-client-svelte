c419728eefe5a78a2e577334e52d783b4dc4d6af
Michael Snook <me@michaelsnook.com>
2021-07-06 17:16:09 +0530

    Freshen up the Banner component

--- src/Banner.svelte
+++ src/Banner.svelte
@@ -1,18 +1,20 @@
 <script>
-  export let bgImage, title, description, smallImage, smallAlt
+  export let bgImage, title, description, smallImage, smallAlt = ''
 </script>
 
 <header
-  class="bg-gray-200 w-full h-96 bg-cover bg-bottom grid"
+  class="bg-gray-500 w-full h-96 bg-cover bg-bottom grid"
   style="background-image: url({bgImage})"
 >
   <div class="container py-10 align-middle place-self-center text-white flex justify-between">
     <div>
-      <h1 class="text-6xl my-4">{title}</h1>
-      <p class="text-xl">{description}</p>
+      <h1 class="h1">{title}</h1>
+      <p class="text-2xl">{description}</p>
     </div>
-    <div>
-      <img class="h-36" src={smallImage} alt={smallAlt} />
-    </div>
+    {#if smallImage}
+      <div>
+        <img class="w-36" src={smallImage} alt={smallAlt} />
+      </div>
+    {/if}
   </div>
 </header>
